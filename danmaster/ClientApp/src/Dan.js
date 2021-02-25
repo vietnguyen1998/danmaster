daba972df0d16dd6b93367da72d7a82186293d74
Viet Nguyen <vnguyen@kicmail.com>
2021-02-25 15:13:27 +0700

    layout

---
+++ danmaster/ClientApp/src/Dan.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+export default () => (
+    <div className="wrapper">
+        <div className="header"> header </div>
+        <div className="left">
+            <div className="left-inner">s</div>
+        </div>
+        <div className="center">
+            <div style={{ height: 200 }}> cc</div>
+            <div style={{ height: 200 }}> cc</div>
+            <div style={{ height: 200 }}> cc</div>
+            <div style={{ height: 200 }}> cc</div>
+            <div style={{ height: 200 }}> cc</div>
+            <div style={{ height: 200 }}> cc</div>
+            <div style={{ height: 200 }}> cc</div>
+        </div>
+        <div className="right">
+            <div className="right-inner"> r</div>
+        </div>
+    </div>
+);
